c9c64dbb96c3285de7c1690f8861972d8335b264
Paulo Oliveira <57108685+OliveiraPauloC@users.noreply.github.com>
2021-06-18 13:07:15 -0300

    Estruturação feita

--- src/components/Repos/index.js
+++ src/components/Repos/index.js
@@ -1,8 +1,38 @@
-import React from 'react' 
+import React,{ useState, useEffect } from 'react'
+import api from '../../services/api'
 
 function Repos() {
+    const [userRepos, setUserRepos] = useState('')
+
+    useEffect(() => {
+        api.get(`users/oliveirapauloc/repos`)
+            .then(res => {
+                setUserRepos(res.data)
+            })
+    }, [])
+
     return (
-        <div>Repos</div>
+        <div>
+            <div>
+                <a href={`/oliveirapauloc`}>
+                    <button>Voltar</button>
+                </a>
+            </div>
+
+            {userRepos && 
+                <div>
+                    {userRepos.map(repo => {
+                        return (
+                            <div key={repo.id}>
+                                <p><b>{repo.name}</b></p>
+                                <p>{repo.description}</p>
+                                <p>{repo.language}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+        </div>
     )
 }
 
--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
-
-import Starred from '../../components/Starred'
-import Repos from '../../components/Repos'
 
 const User = () => {
     const [user, setUser] = useState('')
